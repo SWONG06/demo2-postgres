d7e3f2ec7eb2bab85151bad7e30801657196878c
odenfis <wmoncada@tecsup.edu.pe>
2024-08-27 01:21:58 -0500

    update_mensaje

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ session_start();
       <?php
       
       if (isset($_SESSION['message'])){
-        echo '<div class="alert alert-success" role="alert">' . $_SESSION['message'] . '</div>';
+        echo '<div id="success-message" class="alert alert-success" role="alert">' . $_SESSION['message'] . '</div>';
         unset($_SESSION['message']);
       }
       ?>
@@ -102,9 +102,17 @@ session_start();
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    
-  </body>
+<script>
+  // Espera 5 segundos (5000 milisegundos) y luego esconde el mensaje de éxito
+  setTimeout(function() {
+        var messageDiv = document.getElementById('success-message');
+        if (messageDiv) {
+            messageDiv.style.display = 'none';
+        }
+    }, 5000); // 5000 milisegundos = 5 segundos
+</script>    
+</body>
 </html>
