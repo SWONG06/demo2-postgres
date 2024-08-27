8ccba5f2df4b6746db51467a81110a92dcaded2a
odenfis <wmoncada@tecsup.edu.pe>
2024-08-27 00:55:57 -0500

    update_delete_persona1

--- delete-persona.php
+++ delete-persona.php
@@ -4,7 +4,7 @@ $con = conexion();
 
 $id = $_POST["idpersona"];
 
-$sql = "DELETE FROM persona WHERE id = $1";
+$sql = "DELETE FROM persona WHERE idpersona = $1";
 pg_query_params($con, $sql, array($id));
 
 header("Location: list-personas.php");
