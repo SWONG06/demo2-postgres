740ee419e876b40e8f2ff15fe3913a291b4eff76
odenfis <wmoncada@tecsup.edu.pe>
2024-08-27 00:14:18 -0500

    update_post_index2

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html lang="es">
   <head>
@@ -33,7 +36,7 @@
       <p class="lead">PostgreSQL + PHP</p>
 
       <?php
-      session_start();
+      
       if (isset($_SESSION['message'])){
         echo '<div class="alert alert-success" role="alert">' . $_SESSION['message'] . '</div>';
         unset($_SESSION['message']);
