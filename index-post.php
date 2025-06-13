b8b7f398b87b3d129eb5e9766a15633c3b32e21b
SWONG06 <stephanowong06@gmail.com>
2025-06-12 21:06:14 -0500

    actualizacion de archivos

--- index-post.php
+++ index-post.php
@@ -9,7 +9,7 @@ $dir = $_POST["dir"];
 $cel = $_POST["cel"];
 
 $sql = "INSERT INTO persona VALUES (DEFAULT, '$doc', '$nom', '$ape', '$dir', '$cel')";
-pg_query($conn, $sql); // ← usamos $conn aquí
+pg_query($db, $sql); // ← usamos $conn aquí
 
 session_start();
 $_SESSION['message'] = 'Persona registrada correctamente...';
